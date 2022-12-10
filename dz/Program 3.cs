7caca43f5ae1e9afc012b2e8dbaffbb3492de358
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2022-12-10 20:32:03 +0700

    update program3

--- dz/Program 3.cs
+++ dz/Program 3.cs
@@ -1,3 +1,4 @@
+ //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 Console.Write("Введите  число: ");
 int A = Convert.ToInt32(Console.ReadLine());
 int result = 0;
