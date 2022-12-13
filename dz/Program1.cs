ff0c0a9fb334bf920335d23059267f778e92ff7a
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2022-12-13 23:11:54 +0700

    update program1

--- dz/Program1.cs
+++ dz/Program1.cs
@@ -1,13 +1,25 @@
 // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-Console.Write("Введите число A: ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
-int B = Convert.ToInt32(Console.ReadLine());
-int res = A;
 
-for (int i = 2; i <= B; i++)
+public static class Lesson4
 {
-    res *= A;
-}
+    static void Main()
+    {
+        /// Пользователь вводит число А
+        Console.Write("Введите число A: ");
+    
+        int A = Convert.ToInt32(Console.ReadLine());
+        /// Пользователь вводит число Б
+        Console.Write("Введите число B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+        int res = A;
+        ///Цикл возведения числа А в натуральную степень числа Б
+        for (int i = 2; i <= B; i++)
+        {
+            res *= A;
+        ///Вывод результата вычислений
+        }
+        Console.WriteLine($"{res}");
 
-Console.WriteLine($"{res}");
+    }
+ 
+}
